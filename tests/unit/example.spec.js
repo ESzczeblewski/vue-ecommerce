f704d1bd970a50483cee8773e2ea791030c5e6d3
Kilu691 <kilu691@gmail.com>
2021-04-13 08:54:55 +0200

    Initial commit

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+// import { mount } from '@vue/test-utils';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+// let wrapper;
+
+beforeEach(() => {
+  // wrapper = mount(component, {})
+});
+
+afterEach(() => {
+  // wrapper.destroy();
+});
+
+describe('Desc', () => {
+
 });
