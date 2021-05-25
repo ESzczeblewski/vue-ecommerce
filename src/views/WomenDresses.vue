bdd5558f7d2a7c8b83b53e00a3bbed99e9563421
Kilu691 <kilu691@gmail.com>
2021-05-25 17:33:05 +0200

    Display products category on the top

--- src/views/WomenDresses.vue
+++ src/views/WomenDresses.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="womenDresses view-default">
+    <app-category :category="category"></app-category>
     <app-products-sorting class="view-default__sorting"></app-products-sorting>
-    <app-products :sex="'w'" :category="'Dresses'"></app-products>
+    <app-products :sex="'w'" :category="category"></app-products>
   </div>
 </template>
 
 <script>
 import ProductsGrid from '../components/ProductsGrid.vue';
 import ProductsSorting from '../components/ProductsSorting.vue';
+import Category from '../components/Category.vue';
 
 export default {
   name: 'womenDresses',
   components: {
     appProducts: ProductsGrid,
     appProductsSorting: ProductsSorting,
+    appCategory: Category,
+  },
+  data() {
+    return {
+      category: 'Dresses',
+    };
   },
   beforeDestroy() {
     this.$store.dispatch('RESET_SORTING');
