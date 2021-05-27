c575b20589156761f8bde1d21a21da18073cf5f3
Kilu691 <kilu691@gmail.com>
2021-05-27 22:05:59 +0200

    Implement footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    <p>
+      Powered by <a href="https://wordpress.org/">WordPress.</a> Theme:
+      Shophistic Lite by <a href="https://quemalabs.com/">Quema Labs</a>.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../design/';
+
+.footer {
+  background-color: rgb(56, 56, 56);
+  padding: 2em;
+  color: #919191;
+
+  a {
+    color: rgb(255, 255, 255);
+  }
+}
+</style>
